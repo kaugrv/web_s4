83b932465764c03b00f7f487a5268e438b95db3d
Wendy GERVAIS <kau.grv@gmail.com>
2023-04-11 19:19:08 +0200

    All constellations

--- src/services/api/signRepository.js
+++ src/services/api/signRepository.js
@@ -6,24 +6,28 @@ const getSignData = async function () {
   let signs = ["Aries", "Taurus", "Gemini", "Cancer", "Leo", "Virgo", "Libra", "Scorpio", "Sagittarius", "Capricorn", "Aquarius", "Pisces"]
   let logos = ["♈️", "♉️", "♊️", "♋️", "♌️", "♍️", "♎️", "♏️", "♐️", "♑️", "♒️", "♓️"];
   let urls = []
+  let urls_img = []
   let token = "mmEUtLATc8w_UNnHuR2"
-//   var zodiac = {
-//   "Aries": { "angel": 20, "ra": 3, "de": 20 },
-//   "Taurus": { "angel": 20, "ra": 4, "de": 20 },
-//   "Gemini": { "angel": 20, "ra": 6, "de": 20 },
-//   "Cancer": { "angel": 20, "ra": 8, "de": 20 },
-//   "Leo": { "angel": 20, "ra": 10, "de": 20 },
-//   "Virgo": { "angel": 20, "ra": 12, "de": 20 },
-//   "Libra": { "angel": 20, "ra": 15, "de": 20 },
-//   "Scorpio": { "angel": 20, "ra": 17, "de": 20 },
-//   "Sagittarius": { "angel": 20, "ra": 19, "de": -25 },
-//   "Capricorn": { "angel": 20, "ra": 21, "de": -20 },
-//   "Aquarius": { "angel": 20, "ra": 23, "de": -15 },
-//   "Pisces": { "angel": 20, "ra": 1, "de": -10 }
-// };
+  var zodiac = {
+  "Aries": { "angel": 20, "ra": 3, "de": 20 },
+  "Taurus": { "angel": 20, "ra": 4, "de": 20 },
+  "Gemini": { "angel": 20, "ra": 6, "de": 20 },
+  "Cancer": { "angel": 20, "ra": 8, "de": 20 },
+  "Leo": { "angel": 20, "ra": 10, "de": 20 },
+  "Virgo": { "angel": 20, "ra": 12, "de": 20 },
+  "Libra": { "angel": 20, "ra": 15, "de": 20 },
+  "Scorpio": { "angel": 20, "ra": 17, "de": 20 },
+  "Sagittarius": { "angel": 20, "ra": 19, "de": -25 },
+  "Capricorn": { "angel": 20, "ra": 21, "de": -20 },
+  "Aquarius": { "angel": 20, "ra": 23, "de": -15 },
+  "Pisces": { "angel": 20, "ra": 1, "de": -10 }
+};
+
+ 
 
   signs.forEach(function(sign) {
     urls.push("https://us-central1-tf-natal.cloudfunctions.net/horoscopeapi_test?token=" + token +"&date=today&range&sign="+ sign.toLocaleLowerCase())
+    urls_img.push("http://server2.sky-map.org/map?angel=" + zodiac[sign].angel + "&ra=" + zodiac[sign].ra + "&de=" + zodiac[sign].de + "&show_grid=0&show_const_lines=1&constellation_lines_color=FFFFFF&w=300&h=300&zoom=2");
   });
 
   const tab = [];
@@ -37,7 +41,7 @@ const getSignData = async function () {
         data.sign_name = signs[i];
         data.sign_logo = logos[i];
         data.sign_img = require("../../assets/"+signs[i]+".png");
-        data.sky_img_url = "http://server2.sky-map.org/map?angel=35&ra=19&de=-25&show_grid=0&show_const_lines=1&constellation_lines_color=FFFFFF&constellation="+urls[i]+"&w=300&h=300";
+        data.sky_img_url = urls_img[i];
 
         tab.push(data);
     } else {
